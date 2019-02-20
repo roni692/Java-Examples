105c93e8946eb68d37ecc9aa54473adc3ce7fa45
Ronish <ronishpanjwani@gmail.com>
2019-02-20 13:10:30 -0800

    changes on day3

--- Day2/studentt.java
+++ Day2/studentt.java
@@ -27,12 +27,24 @@ public class studentt
 		System.out.println("Enter Student age");
 		int age = sc.nextInt(); 
 		student.setage(age);
+		
 		//System.out.println("Student fees is");
 		//long fees = sc.nextLong();
 		System.out.println("Student Name is :"+student.getstudentName());
 		System.out.println("Residential Status :"+student.getResidentialStatus());
 		System.out.println("Stduent age :"+student.getage());
 		System.out.println("Student Fees: "+student.getfeesPerMonth());
+		if(student.getResidentialStatus().equals("Hostilite"))
+		{
+			student.calculateFees(10000,20000);
+			System.out.println("blah");
+		}
+		else
+		{
+			student.calculateFees(10000);
+			
+		}
+		//System.out.println("Fees"student.calculateFees(semesterFees););
 		
 	}	
 	public  void setage(int a)
@@ -75,17 +87,17 @@ public class studentt
 	{
 		return 200000;
 	}
-	void calculateFees(double semesterFees)
+	void calculateFees(int semesterFees)
 	{
 		feesPerMonth = (long) (semesterFees/6.0);
-		System.out.println("Student fees without hostel"+feesPerMonth);
+		System.out.println("Student fees without hostel "+feesPerMonth);
 		
 	}
-	void calculateFees(double semesterFees, double hostelFees)
+	void calculateFees(int semesterFees, int hostelFees)
 	{
 		feesPerMonth = (long)(semesterFees/6.0);
 		feesPerMonth =(long)(feesPerMonth + hostelFees);
-		System.out.println("Student fees with hostel"+feesPerMonth);
+		System.out.println("Student fees with hostel "+feesPerMonth);
 		
 	}
 }
